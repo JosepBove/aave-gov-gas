f8947ff5c7f22dd80e4a13554c84b0bc5ce87059
JosepBove <josep@aave.com>
2023-05-30 20:29:16 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# aave-gov-gas
+
+Script to get the gas used form block a to b on the governance contract of Aave according to an input file with information of the delegators.
+
+
+## How to use
+
+1. Run `npm i`
+2. Copy `.env.example` and name it `.env`
+3. Set your env variables
+
+It works without an Etherscan API key but it will be very very slow
+
+## How to add yourself as a delegator
+
+Just edit the input.json file and add your name and all your signer addresses! (If you are an EOA you can just add your address there)
+
---
+++ index.js
@@ -0,0 +1,96 @@
+const ethers = require("ethers");
+const fs = require('fs').promises;
+require("dotenv").config();
+
+let provider = new ethers.providers.EtherscanProvider(
+    ethers.providers.getNetwork("homestead"),
+    process.env.ETHERSCAN_API_KEY
+);
+
+async function getGasCosts(delegates) {
+
+    let history = await provider.getHistory(
+        process.env.AAVE_GOVERNANCE_V2,
+        process.env.FROM_BLOCK,
+        process.env.TO_BLOCK
+    );
+
+    let receipt;
+    let delegateIndex;
+    let signerIndex; 
+
+    for(let i = 0; i < history.length; ++i) {
+       
+        delegateIndex = -1;
+        signerIndex = -1;
+
+        receipt = (await provider.getTransactionReceipt(history[i].hash));
+
+        for(let j = 0; j < delegates.length; j++) {
+            for(let k = 0; k < delegates[j].signers.length; k++) {
+                if(delegates[j].signers[k].address === receipt.from) {
+                    delegateIndex = j;
+                    signerIndex = k;
+                }
+            }
+        }
+
+        if(delegateIndex != -1) {
+            let gas = receipt.gasUsed.mul(receipt.effectiveGasPrice);
+            
+            if(delegates[delegateIndex].signers[signerIndex].gasUsed) {
+                delegates[delegateIndex].signers[signerIndex].gasUsed = delegates[delegateIndex].signers[signerIndex].gasUsed.add(gas);
+            } else {
+                delegates[delegateIndex].signers[signerIndex].gasUsed = gas;
+            }
+        }
+    }
+    return delegates;
+
+}
+
+async function parseDelegates() {
+    let data = await fs.readFile('input.json', 'utf-8');
+    let obj = JSON.parse(data);
+
+    obj.map(x => 
+        x.signers = x.signers.map( e =>
+            e = {
+                address: e,
+                gasUsed: 0 
+            }
+        )
+    );
+
+    return obj;
+}
+
+async function writeOutput(output) {
+    
+    for(let i = 0; i < output.length; i++) {
+
+        let sum = ethers.BigNumber.from('0');
+        
+        for(let j = 0; j < output[i].signers.length; j++) {
+            sum =  sum.add(output[i].signers[j].gasUsed);
+            output[i].signers[j].gasUsed ?  output[i].signers[j].gasUsed = ethers.utils.formatEther(output[i].signers[j].gasUsed): 0; 
+        }
+        output[i].gasUsed = ethers.utils.formatEther(sum);
+    }
+
+    await fs.writeFile('output.json', JSON.stringify(output, null, '\t'));
+}
+
+async function main() {
+    console.log("Checking transactions please wait");
+
+    let delegates = await parseDelegates();
+    
+    let output = await getGasCosts(delegates);
+    
+    await writeOutput(output);
+
+    console.log("output.json written");   
+}
+
+main();
